16438ab00addeef45a8ef2655b3de636e22b436a
IAmVengeance <ayan.saxena24@gmail.com>
2024-12-24 20:28:04 +0530

    Changed dimensions

--- src/Repository.jsx
+++ src/Repository.jsx
@@ -413,7 +413,7 @@ const Repository = () => {
         }}
       />
             </div>
-            <div className="flex flex-col h-[95%] sm:justify-between mt-4 pb-4">
+            <div className="flex flex-col h-[97%] 2xl:h-[95%] sm:justify-between mt-4 pb-4">
               <div className="flex flex-col sm:w-[80%] xl:mr-8">
                 <CustomButton>
                   <>
